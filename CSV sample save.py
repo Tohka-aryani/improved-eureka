92f1cc1a04589c6ef0c72b134f3b888d89a61712
Tohka-aryani <129815399+Tohka-aryani@users.noreply.github.com>
2023-06-11 02:55:25 +0800

    Create CSV sample save.py

---
+++ CSV sample save.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import pandas as pd
+import git
+
+# Form
+name = st.text_input("Name")
+age = st.number_input("Age")
+submit = st.button("Submit")
+
+if submit:
+    # Create DataFrame
+    data = {'Name': [name], 'Age': [age]}
+    df = pd.DataFrame(data)
+
+    # Save as CSV
+    df.to_csv('data.csv', index=False)
+
+    # Clone GitHub repository
+    git.Repo.clone_from('https://github.com/your_username/your_repository.git', 'local_repo')
+
+    # Move CSV to repository folder
+    repo_path = 'local_repo'
+    file_path = 'data.csv'
+    new_file_path = f'{repo_path}/{file_path}'
+    shutil.move(file_path, new_file_path)
+
+    # Commit and push changes
+    repo = git.Repo(repo_path)
+    repo.git.add(new_file_path)
+    repo.git.commit('-m', 'Added data.csv')
+    repo.git.push()
