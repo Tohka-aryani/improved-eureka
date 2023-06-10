5893f9402bc4300c7bcc61a6908a9f351fc2f124
Tohka-aryani <129815399+Tohka-aryani@users.noreply.github.com>
2023-06-11 02:51:55 +0800

    Update V2.py

--- V2.py
+++ V2.py
@@ -1,81 +1,79 @@
 import streamlit as st
-import pandas as pd
 
-def create_application_form():
-    """
-    This function creates an application form with a viewable and editable database using Streamlit.
-    The form includes fields for Twitch username, Discord username, and reason for applying to the streamer's Twitch team.
-    It also includes checkboxes for enabled VODs on Twitch, having Twitch panels, and linking Twitch to Discord.
-    A T&C checkbox with customizable text is included, as well as a dropdown selection for Twitch status field.
-    A separate page is included for limited people to edit the database on Streamlit.
-    """
-    # Create a dictionary of the form fields
-    form_fields = {
-        "Twitch username": "",
-        "Discord username": "",
-        "Reason for applying into MY Streamers Twitch Team": "",
-        "Enabled VODs on Twitch": False,
-        "Have Twitch Panels": False,
-        "Linked Twitch to Discord": False,
-        "T&C checkbox": False,
-        "Twitch status": ["Affiliate", "Partner", "None"]
-    }
-    
-    # Create the form using Streamlit
+def main():
+    st.set_page_config(layout="wide")
     st.title("Application Form")
-    st.write("Please fill out the following information to apply to MY Streamers Twitch Team.")
-    st.write("All fields are required unless otherwise noted.")
-    
-    # Add the form fields to the Streamlit app
-    form_values = {}
-    for field, default_value in form_fields.items():
-        if isinstance(default_value, bool):
-            form_values[field] = st.checkbox(field, value=default_value)
-        elif isinstance(default_value, list):
-            form_values[field] = st.selectbox(field, options=default_value)
-        else:
-            form_values[field] = st.text_input(field, value=default_value)
-    
-    # Add the T&C checkbox
-    form_values["T&C checkbox"] = st.checkbox("I agree to the terms and conditions.")
-    
-    # Add a submit button
+
+    # Sidebar navigation
+    page = st.sidebar.selectbox("Page", ["Form", "View Submissions"])
+
+    if page == "Form":
+        show_application_form()
+    elif page == "View Submissions":
+        show_submissions()
+
+def show_application_form():
+    st.header("Application Form")
+
+    # Paragraph fields
+    twitch_name = st.text_input("Twitch Name")
+    discord_name = st.text_input("Discord Name")
+    reason = st.text_area("Reason for Application")
+
+    # Checkboxes
+    connected_twitch_discord = st.checkbox("Connected Twitch to Discord")
+    enabled_vods = st.checkbox("Enabled VODs")
+    twitch_panels = st.checkbox("Twitch Panels")
+
+    # Terms and Conditions agreement
+    terms_accepted = st.checkbox("I agree to the Terms and Conditions")
+
     if st.button("Submit"):
-        # Check if all required fields are filled
-        if "" in form_values.values():
-            st.warning("Please fill out all required fields.")
-        elif not form_values["T&C checkbox"]:
-            st.warning("Please agree to the terms and conditions.")
+        if not terms_accepted:
+            st.error("You must agree to the Terms and Conditions to submit the form.")
         else:
-            # Add the form data to the database
-            df = pd.DataFrame([form_values])
-            st.write("Thank you for applying! Your application has been submitted.")
+            # Process the form submission
+            # You can perform further validation or save the form data to a database
+
+            st.success("Form submitted successfully!")
+
+def show_submissions():
+    st.header("View Submissions")
+
+    # In this example, we use placeholder data to simulate submitted applications
+    submissions = [
+        {
+            "Twitch Name": "user1",
+            "Discord Name": "user1#1234",
+            "Reason for Application": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+            "Connected Twitch to Discord": True,
+            "Enabled VODs": False,
+            "Twitch Panels": True
+        },
+        {
+            "Twitch Name": "user2",
+            "Discord Name": "user2#5678",
+            "Reason for Application": "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+            "Connected Twitch to Discord": True,
+            "Enabled VODs": True,
+            "Twitch Panels": False
+        }
+    ]
+
+    if len(submissions) == 0:
+        st.info("No submissions found.")
+    else:
+        for i, submission in enumerate(submissions):
+            st.subheader(f"Application {i+1}")
+            col1, col2 = st.beta_columns(2)
+            col1.write(f"Twitch Name: {submission['Twitch Name']}")
+            col1.write(f"Discord Name: {submission['Discord Name']}")
+            col2.write(f"Reason for Application: {submission['Reason for Application']}")
+            col2.write(f"Connected Twitch to Discord: {submission['Connected Twitch to Discord']}")
+            col2.write(f"Enabled VODs: {submission['Enabled VODs']}")
+            col2.write(f"Twitch Panels: {submission['Twitch Panels']}")
+
+if __name__ == '__main__':
+    main()
+
     
-    # Add a separate page for limited people to edit the database
-    if st.sidebar.checkbox("Edit Database"):
-        # Authenticate the user
-        password = st.sidebar.text_input("Password", type="password")
-        if password == "password123":
-            # Load the database
-            df = pd.read_csv("database.csv")
-            
-            # Display the database
-            st.write("Database")
-            st.write(df)
-            
-            # Allow the user to edit the database
-            st.write("Add a new entry")
-            new_entry = {}
-            for field in form_fields.keys():
-                if isinstance(form_fields[field], bool):
-                    new_entry[field] = st.checkbox(field)
-                elif isinstance(form_fields[field], list):
-                    new_entry[field] = st.selectbox(field, options=form_fields[field])
-                else:
-                    new_entry[field] = st.text_input(field)
-            if st.button("Add"):
-                df = pd.concat([df, pd.DataFrame([new_entry])])
-                df.to_csv("database.csv", index=False)
-                st.write("Entry added to database.")
-        else:
-            st.warning("Incorrect password.")
