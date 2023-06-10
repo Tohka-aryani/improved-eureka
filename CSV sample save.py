7c2ca65463b04897b52a5fd3eebb6258a6ec06ef
Tohka-aryani <129815399+Tohka-aryani@users.noreply.github.com>
2023-06-11 03:06:42 +0800

    Update CSV sample save.py

--- CSV sample save.py
+++ CSV sample save.py
@@ -1,31 +1,40 @@
 import streamlit as st
 import pandas as pd
-import git
+import base64
 
-# Form
-name = st.text_input("Name")
-age = st.number_input("Age")
-submit = st.button("Submit")
+# Set up the page layout
+st.set_page_config(layout="wide")
 
+# Create a title and subheader
+st.title("GitHub CSV Data App")
+st.subheader("Data Entry and Saving")
+
+# Create a form to collect data
+name = st.text_input("Enter a name")
+age = st.number_input("Enter an age")
+email = st.text_input("Enter an email")
+submit = st.button("Add Entry")
+
+# Load the existing data from GitHub CSV file
+data_url = "https://raw.githubusercontent.com/<username>/<repository>/main/data.csv"
+df = pd.read_csv(data_url)
+
+# Display the existing data
+st.subheader("Existing Data")
+st.dataframe(df)
+
+# Add new entry to the DataFrame
 if submit:
-    # Create DataFrame
-    data = {'Name': [name], 'Age': [age]}
-    df = pd.DataFrame(data)
+    new_entry = {'Name': name, 'Age': age, 'Email': email}
+    df = df.append(new_entry, ignore_index=True)
+    st.success("New entry added successfully!")
 
-    # Save as CSV
-    df.to_csv('data.csv', index=False)
+# Save the updated data to a new CSV file
+csv = df.to_csv(index=False)
+b64 = base64.b64encode(csv.encode()).decode()
+file_name = "data.csv"
+href = f'<a href="data:file/csv;base64,{b64}" download="{file_name}">Download CSV File</a>'
+st.markdown(href, unsafe_allow_html=True)
 
-    # Clone GitHub repository
-    git.Repo.clone_from('https://github.com/Tohka-aryani/improved-eureka/tree/main', 'local_repo')
-
-    # Move CSV to repository folder
-    repo_path = 'local_repo'
-    file_path = 'data.csv'
-    new_file_path = f'{repo_path}/{file_path}'
-    shutil.move(file_path, new_file_path)
-
-    # Commit and push changes
-    repo = git.Repo(repo_path)
-    repo.git.add(new_file_path)
-    repo.git.commit('-m', 'Added data.csv')
-    repo.git.push()
+if __name__ == '__main__':
+    st.run_app()
