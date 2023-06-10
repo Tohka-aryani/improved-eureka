a0b850990d06613b2ce2632415f7c1390c848f3a
Tohka-aryani <129815399+Tohka-aryani@users.noreply.github.com>
2023-06-11 03:23:22 +0800

    Update V2.py

--- V2.py
+++ V2.py
@@ -1,18 +1,19 @@
 import streamlit as st
+import pandas as pd
 
 def main():
     st.title("Application Form")
     
     # Page navigation
-    page = st.sidebar.selectbox("Page", ("Form", "View Data"))
+    page = st.sidebar.radio("Page", ("Form", "View Data"))
     
     if page == "Form":
-        show_form()
+        show_application_form()
     elif page == "View Data":
-        show_data()
+        show_data_table()
 
-def show_form():
-    st.subheader("Application Form")
+def show_application_form():
+    st.header("Submit Application")
     
     # Paragraph fields
     twitch_name = st.text_input("Twitch Name")
@@ -44,24 +45,30 @@ def show_form():
             st.session_state.submitted_data = {
                 "Twitch Name": twitch_name,
                 "Discord Name": discord_name,
-                "Reason for application": reason,
+                "Reason for Application": reason,
                 "Connected Twitch to Discord": connected,
                 "Enabled VODs": enabled_vods,
                 "Twitch Panels": twitch_panels
             }
-            st.session_state.form_submitted = True
             st.success("Your application has been submitted successfully!")
 
-def show_data():
-    st.subheader("Submitted Data")
+def show_data_table():
+    st.header("Submitted Applications")
     
-    if not hasattr(st.session_state, "form_submitted") or not st.session_state.form_submitted:
-        st.warning("No data has been submitted yet.")
+    # Check if there is submitted data
+    if "submitted_data" not in st.session_state:
+        st.warning("No applications have been submitted yet.")
     else:
-        submitted_data = st.session_state.submitted_data
-        for field, value in submitted_data.items():
-            st.write(f"**{field}:** {value}")
+        # Get the submitted data
+        data = st.session_state.submitted_data
+        
+        # Create a DataFrame from the submitted data
+        df = pd.DataFrame([data])
+        
+        # Display the data in a table
+        st.dataframe(df)
 
 if __name__ == "__main__":
     main()
+
 
