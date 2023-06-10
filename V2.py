1f5d32662d64b7ac2e8cd81fc87a77c784d9cdd4
Tohka-aryani <129815399+Tohka-aryani@users.noreply.github.com>
2023-06-11 03:19:32 +0800

    Update V2.py

--- V2.py
+++ V2.py
@@ -1,6 +1,14 @@
 import streamlit as st
 
 def main():
+    page = st.sidebar.selectbox("Select a page", ["Application Form", "View Submitted Data"])
+    
+    if page == "Application Form":
+        display_application_form()
+    elif page == "View Submitted Data":
+        display_submitted_data()
+
+def display_application_form():
     st.title("Application Form")
     
     # Paragraph fields
@@ -30,16 +38,36 @@ def main():
         else:
             # Process the form data (e.g., send it to a backend, store it in a database, etc.)
             # Replace the print statements with your desired processing logic
-            print("Twitch Name:", twitch_name)
-            print("Discord Name:", discord_name)
-            print("Reason for application:", reason)
-            print("Connected Twitch to Discord:", connected)
-            print("Enabled VODs:", enabled_vods)
-            print("Twitch Panels:", twitch_panels)
+            st.session_state.submissions.append({
+                "Twitch Name": twitch_name,
+                "Discord Name": discord_name,
+                "Reason for application": reason,
+                "Connected Twitch to Discord": connected,
+                "Enabled VODs": enabled_vods,
+                "Twitch Panels": twitch_panels
+            })
             st.success("Your application has been submitted successfully!")
 
-if __name__ == "__main__":
-    main()
+def display_submitted_data():
+    st.title("Submitted Data")
+    
+    # Display the submitted data
+    submissions = st.session_state.submissions
+    if not submissions:
+        st.info("No data submitted yet.")
+    else:
+        selected_submissions = st.multiselect("Select entries", submissions, format_func=lambda submission: submission["Twitch Name"])
+        if selected_submissions:
+            st.write("Selected Entries:")
+            for submission in selected_submissions:
+                st.write(submission)
+        else:
+            st.write("All Entries:")
+            for submission in submissions:
+                st.write(submission)
 
+if __name__ == "__main__":
+    st.session_state.submissions = []  # Initialize the list to store the submissions
+    main()
 
     
