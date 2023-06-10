a1840ee3fe0ed082f7a64e02506f9dac967eead9
Tohka-aryani <129815399+Tohka-aryani@users.noreply.github.com>
2023-06-11 02:03:07 +0800

    Update code.py

--- code.py
+++ code.py
@@ -1,79 +1,65 @@
 import streamlit as st
 import pandas as pd
 
-# Define the application form fields
-application_form = {
-    'Name': '',
-    'Email': '',
-    'Twitch Status': '',
-    'Application Status': ''
-}
+# Create a DataFrame to store the application data
+data = pd.DataFrame(columns=['Name', 'Email', 'Twitch Status', 'Application Status'])
 
-# Create a DataFrame to store the applications
-database = pd.DataFrame(columns=application_form.keys())
-
-# Dropdown options for Twitch Status
+# Dropdown options
 twitch_status_options = ['Affiliate', 'Partner', 'None']
+application_status_options = ['Accepted', 'Pending', 'Rejected']
 
-# Dropdown options for Application Status
-application_status_options = ['Pending', 'Accepted', 'Rejected']
-
-# Define the Streamlit layout
+# Streamlit application
 def main():
-    # Title of the application form
-    st.title('Application Form')
+    st.title("Application Form")
     
-    # Display the form fields
-    with st.form(key='application_form'):
-        col1, col2 = st.beta_columns(2)
-        with col1:
-            application_form['Name'] = st.text_input('Name')
-            application_form['Email'] = st.text_input('Email')
-        with col2:
-            application_form['Twitch Status'] = st.selectbox('Twitch Status', twitch_status_options)
-            application_form['Application Status'] = st.selectbox('Application Status', application_status_options)
-        
-        # Submit button
-        submitted = st.form_submit_button('Submit')
+    st.write("Please fill out the following information:")
     
-    if submitted:
-        # Append the form data to the database
-        database.loc[len(database)] = application_form
-        
-        # Clear the form fields
-        for field in application_form:
-            application_form[field] = ''
+    # Form input fields
+    name = st.text_input("Name")
+    email = st.text_input("Email")
+    twitch_status = st.selectbox("Twitch Status", twitch_status_options)
+    application_status = st.selectbox("Application Status", application_status_options)
     
-    # Display the database
-    st.title('Database')
-    st.dataframe(database)
+    # Submit button
+    if st.button("Submit"):
+        # Add data to the DataFrame
+        data.loc[len(data)] = [name, email, twitch_status, application_status]
+        st.success("Application submitted successfully!")
     
-    # Display the editable page
-    if st.sidebar.button('Edit Database'):
-        edit_database()
-        
+    # Display the current database
+    st.subheader("Current Applications")
+    st.dataframe(data)
+    
+# Page for limited people to edit the database
+def admin_page():
+    st.title("Admin Page")
+    
+    # Authenticate admin access
+    password = st.text_input("Password", type="password")
+    if password != "admin123":
+        st.error("Incorrect password!")
+        return
+    
+    # Display the editable database
+    st.subheader("Edit Applications")
+    st.dataframe(data)
+    
+    # Allow editing of the application status
+    application_index = st.number_input("Enter the index of the application to edit", min_value=0, max_value=len(data)-1, value=0)
+    new_status = st.selectbox("Update Application Status", application_status_options)
+    
+    # Update the application status upon button click
+    if st.button("Update"):
+        data.loc[application_index, 'Application Status'] = new_status
+        st.success("Application status updated successfully!")
+    
+# Navigation menu
+menu = ["Application Form", "Admin Page"]
+choice = st.sidebar.selectbox("Menu", menu)
 
-# Editable page to update the database
-def edit_database():
-    st.title('Edit Database')
-    
-    # Display the database as a table with editable cells
-    database_editable = database.copy()
-    for column in database_editable.columns:
-        database_editable[column] = database_editable[column].astype(str)
-    st.dataframe(database_editable, editable=True)
-    
-    # Save changes to the database
-    if st.button('Save Changes'):
-        st.dataframe(database_editable)
-        # You can update the original database here
-    
-    # Reset the database
-    if st.button('Reset Database'):
-        database_editable = pd.DataFrame(columns=application_form.keys())
-        st.dataframe(database_editable)
-
-
-if __name__ == '__main__':
+if choice == "Application Form":
     main()
+elif choice == "Admin Page":
+    admin_page()
+
 
